3376d4294106491a3bab4730eea11e1c14e6dac9
sebastian <sebastianurbina.idetp@gmail.com>
2023-08-11 08:18:42 -0500

    se cambia la conexion de la base de datos con postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksTypesModule } from './tasks_types/tasks_types.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type:'mysql',
-    host:'localhost',
-    port:3306,
-    username:'sebas_cruds',
-    password: 'root',
-    database: 'task_db',
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Cargar las variables de entorno globalmente
+    }),
+    TypeOrmModule.forRoot({
+    type:'postgres',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    username:process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     autoLoadEntities: true,
     synchronize: true,
   }),TasksModule, TasksTypesModule, UsersModule, AuthModule],
